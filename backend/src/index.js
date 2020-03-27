7e1a553d87b03b284d37859c2a3e4a5850c586d1
Alex Ferman <alexjferman@gmail.com>
2020-03-27 13:36:14 -0400

    startded backend

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require ('cors');
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(3333)
+
+
+
+
+/* 
+*Rotas e recursos /users 
+#Metodos HTTP
+Get:buscar
+Post: criar 
+PUt: alterar
+Delete: remove
+*/
+/* tipos de parametros
+Querry: parametros nomeados enviados na rota simbolo de ? (filtro de paginação)
+Route : parametro para inentificar recursos.
+request body: corpo da requisição cria e alterar o recurso
+
+*/
+/* Banco de dados */
+//sql-relacional
+//NoSql
+
+/* instal db */
---
+++ backend/src/routes.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+
+const routes = express.Router();
+routes.post('/session', SessionController.create);
+
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+//Rotas para os incidents
+routes.get('/incidents',IncidentsController.index);
+routes.post('/incidents',IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
+
+//Routes Profile
+routes.get('/profile', ProfileController.index);
+
+
+module.exports = routes; 
